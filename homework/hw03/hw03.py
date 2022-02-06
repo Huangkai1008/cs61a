58fd046c407d6f8ab3b92ea99816c96e54db5209
huangkai <h1770360848@outlook.com>
2022-02-06 22:24:22 +0800

    feat(hw): perf hw03 Q5

--- homework/hw03/hw03.py
+++ homework/hw03/hw03.py
@@ -226,7 +226,15 @@ def move_stack(n, start, end):
     Move the top disk from rod 1 to rod 3
     """
     assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
-    "*** YOUR CODE HERE ***"
+
+    # base case
+    if n == 1:
+        print_move(start, end)
+    else:
+        # recursive case
+        move_stack(n - 1, start, 6 - start - end)
+        move_stack(1, start, end)
+        move_stack(n - 1, 6 - start - end, end)
 
 
 from operator import sub, mul
